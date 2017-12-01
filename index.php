347cbb351ab780564b980648e5de50f6cad65bab
Romain Gilleron <natasfh@gmail.com>
2017-12-01 12:39:30 +0100

    kikoolol

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php // index.php
+include('inc/db.php');
+include('inc/fonctions.php');
+
+?>
+
+<h1>bonjoir</h1>
+
+<p>bienvenue sur michel</p>
+
+
+<?php include('inc/header.php'); ?>
+
+
+<p>hips</p>
+
+
+
+<?php include('inc/footer.php');
